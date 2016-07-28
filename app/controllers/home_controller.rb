4c923cb9934c277d86d132f3463c94f4681ac789
Nathan He <nathan.he@pearson.com>
2016-07-28 12:33:40 -0500

    Simplify and remove debugging code

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,37 +3,20 @@ class HomeController < ApplicationController
   end
 
   def verify
-    if verify_wechat_request
-      @echo_str = params[:echostr]
-    else
-      @echo_str = ''
-    end
-    render text: @echo_str, content_type: 'text/plain'
+    render plain: wechat_request_isvalid ? params[:echostr] : ''
   end
 
   private
 
-  def verify_wechat_request
+  def wechat_request_isvalid
     require 'digest/sha1'
 
     signature = params[:signature]
     timestamp = params[:timestamp]
     nonce = params[:nonce]
-
-    puts "signature -> #{signature}"
-    puts "timestamp -> #{timestamp}"
-    puts "nonce -> #{nonce}"
-
     token = "abcdefghijklmnop"
-
     tmp_str = [token, timestamp, nonce].sort.join
-    puts "tmp_str -> #{tmp_str}"
-
-    hash_str = Digest::SHA1.hexdigest tmp_str
-    puts "hash_str -> #{hash_str}"
-
-    puts "#{hash_str} = #{signature} -> #{hash_str == signature}"
-
-    return hash_str == signature
+    hash_str = Digest::SHA1.hexdigest(tmp_str)
+    hash_str == signature
   end
 end
