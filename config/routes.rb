14b8fd40db33bbaa0924efa78f38115ae8ebc40a
Nathan He <nathan.he@pearson.com>
2016-07-28 11:25:55 -0500

    Add verification method to verify wechat api config

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
-  get 'home/verify'
-
   root 'home#index'
+
+  get '/verify', to:'home#verify'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
